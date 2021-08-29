e3c197762adc2a934bdbb06376cedf3d1af8cece
MarcosAntfon <fs37158@gmail.com>
2021-08-29 14:48:44 -0300

    first commit

--- Git/Primeira/src/PrimeiraAula.java
+++ Git/Primeira/src/PrimeiraAula.java
@@ -1,13 +1,20 @@
+import com.SegundaAula;
 
 public class PrimeiraAula {
 
 	public static void main(String[] args) {
 
-	int a = 5;
-	int b = 9;
-	int c = 6;
+		PrimeiraAula primeiraaula = new PrimeiraAula();
+		SegundaAula segundaaula = new SegundaAula(); 
+		
+		System.out.println(primeiraaula);
+		System.out.println(segundaaula);
+
+		/*int a = 5;
+		int b = 9;
+		int c = 6;
 	
-	System.out.println("Helo World! " + (a+b*c));
+	System.out.println("Helo World! " + (a+b*c));*/
 	
 	}
 

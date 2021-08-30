6c609dd8de17ad0a709ddfc2a9eff737f9e0547c
MarcosAntfon <fs37158@gmail.com>
2021-08-30 10:30:22 -0300

    SegundaAula

--- Git/Primeira/src/PrimeiraAula.java
+++ Git/Primeira/src/PrimeiraAula.java
@@ -1,15 +1,13 @@
-import com.SegundaAula;
 
 public class PrimeiraAula {
+
 
 	public static void main(String[] args) {
 
 		PrimeiraAula primeiraaula = new PrimeiraAula();
-		SegundaAula segundaaula = new SegundaAula(); 
 		
 		System.out.println(primeiraaula);
-		System.out.println(segundaaula);
-
+		
 		/*int a = 5;
 		int b = 9;
 		int c = 6;
--- Git/Primeira/src/SegundaAula.java
+++ Git/Primeira/src/SegundaAula.java
@@ -1,5 +1,3 @@
-package com;
-
 import java.util.Objects;
 
 public class SegundaAula {
